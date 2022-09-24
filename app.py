e540ae25779fc98794d75fc347cfd7f88c17fdda
Marcos Bernard <marcoscalixto92@gmail.com>
2022-09-23 21:30:31 -0500

    plot estático

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
+import streamlit as st
 import matplotlib.pyplot as plt
-import mpld3
-import streamlit.components.v1 as components
 
 #create your figure and get the figure object returned
 fig = plt.figure()
 plt.plot([1,2,3,4,5])
 
-fig_html = mpld3.fig_to_html(fig)
-components.html(fig_html, height=600) 
+st.pyplot(fig)
