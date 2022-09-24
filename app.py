e93b01a3b109f508d6168231461e4fb7fe015d5a
Marcos Bernard <marcoscalixto92@gmail.com>
2022-09-23 23:37:31 -0500

    _

--- app.py
+++ app.py
@@ -76,35 +76,26 @@ plt.plot(t2, np.cos(2*np.pi*t2), color='tab:orange',linestyle='--',marker='.')
 fig_html = mpld3.fig_to_html(two_subplot_fig)
 components.html(fig_html, height=600)
 '''
-#Imports for all of the code
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, mpld3
 import numpy as np
-import pandas as pd
-import mpld3
-import streamlit as st
 from mpld3 import plugins
 import streamlit.components.v1 as components
-
 def f(t):
-    return np.exp(-t)*np.cos(2*np.pi*t)
+    return np.exp(-t) * np.cos(2*np.pi*t)
 
-t1 = np.arange(0.0,5.0,0.1)
-t2 = np.arange(0.0,5.0,0.02)
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
 
-# How to set the graph size
-two_subplot_fig = plt.figure(figsize=(6,6))
+two_subplot_fig = plt.figure(figsize=(8,8))
 plt.subplot(211)
-plt.plot(t1,f(t1),color='tab:blue',marker=',')
-plt.plot(t2,f(t2),color='black',marker='.')
+plt.plot(t1, f(t1), color='tab:blue', marker=',')
+plt.plot(t2, f(t2), color='black', marker='.')
 
 plt.subplot(212)
-plt.plot(t2, np.cos(2*np.pi*t2), color='tab:orange',linestyle='--',marker='.')
+plt.plot(t2, np.cos(2*np.pi*t2), color='tab:orange', linestyle='--', marker='.')
 
-fig_html = mpld3.fig_to_html(two_subplot_fig)
-components.html(fig_html, height=600)
-# CODE TO ADD
 # Define some CSS to control our custom labels
-css = """
+css = '''
 table
 {
   border-collapse: collapse;
@@ -124,17 +115,17 @@ table, th, td
   border: 1px solid black;
   text-align: right;
 }
-"""
+'''
+
 for axes in two_subplot_fig.axes:
-    for line in axes.get_lines():
-        # get the x and y coords
-        xy_data = line.get_xydata()
-        labels = []
-        for x, y in xy_data:
-            # Create a label for each point with the x and y coords
-            html_label = f'<table border="1" class="dataframe"> <thead> <tr style="text-align: right;"> </thead> <tbody> <tr> <th>x</th> <td>{x}</td> </tr> <tr> <th>y</th> <td>{y}</td> </tr> </tbody> </table>'
-            labels.append(html_label)
-        # Create the tooltip with the labels (x and y coords) and attach it to each line with the css specified
-        tooltip = plugins.PointHTMLTooltip(line, labels, css=css)
-        # Since this is a separate plugin, you have to connect it
-        plugins.connect(two_subplot_fig, tooltip)
+  for line in axes.get_lines():
+    xy_data = line.get_xydata()
+    labels = []
+    for x,y in xy_data:
+      html_label = f'<table border="1" class="dataframe"> <thead> <tr style="text-align: right;"> </thead> <tbody> <tr> <th>x</th> <td>{x}</td> </tr> <tr> <th>y</th> <td>{y}</td> </tr> </tbody> </table>'
+      labels.append(html_label)
+    tooltip = plugins.PointHTMLTooltip(line, labels, css=css)
+    plugins.connect(two_subplot_fig, tooltip)
+
+fig_html = mpld3.fig_to_html(two_subplot_fig)
+components.html(fig_html, height=850, width=850)
