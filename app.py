7dc9871e637a238d738e770a54582337d63e1293
Marcos Bernard <marcoscalixto92@gmail.com>
2022-09-23 19:46:20 -0500

    _

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import mpld3
 import streamlit.components.v1 as components
 
-
 #create your figure and get the figure object returned
-fig = plt.figure(figsize=(12,8))
+fig = plt.figure()
 plt.plot([1,2,3,4,5])
 
 fig_html = mpld3.fig_to_html(fig)
