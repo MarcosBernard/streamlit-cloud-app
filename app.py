962f744ac4b288d9ebacd0430fc46aa512e5711b
Marcos Bernard <marcoscalixto92@gmail.com>
2022-09-23 23:29:28 -0500

    hello

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ plt.plot([1,2,3,4,5])
 fig_html = mpld3.fig_to_html(fig)
 components.html(fig_html, height=600)
 '''
+'''
 #Imports for all of the code
 import matplotlib.pyplot as plt
 import numpy as np
@@ -46,3 +47,30 @@ plt.subplot(212)
 plt.plot(t2, np.cos(2*np.pi*t2), color='tab:orange',linestyle='--',marker='.')
 
 st.pyplot(two_subplot_fig)
+'''
+#Imports for all of the code
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mpld3
+import streamlit as st
+from mpld3 import plugins
+import streamlit.components.v1 as components
+
+def f(t):
+    return np.exp(-t)*np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0,5.0,0.1)
+t2 = np.arange(0.0,5.0,0.02)
+
+# How to set the graph size
+two_subplot_fig = plt.figure(figsize=(6,6))
+plt.subplot(211)
+plt.plot(t1,f(t1),color='tab:blue',marker=',')
+plt.plot(t2,f(t2),color='black',marker='.')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), color='tab:orange',linestyle='--',marker='.')
+
+fig_html = mpld3.fig_to_html(two_subplot_fig)
+components.html(fig_html, height=600)
